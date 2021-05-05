b6b85016e365fb8f29f6bca0a692f29967895121
group7BSE1 <group7bse1>
2021-05-05 15:05:27 +0300

    Final version of the programme

--- mid_term/project.py
+++ mid_term/project.py
@@ -1,7 +1,7 @@
 def main():
     while True:
         code = input("\nEnter customer code: ")
-        if code == "r".upper() or code =="c".upper() or code == "i".upper():
+        if code == "r" or code == "R" or code == "c" or code == "C" or code == "i" or code == "I":
             beg_m = input("Enter beginning meter reading: ")
             if int(beg_m) < 0:
                 print("Invalid meter reading")
@@ -10,18 +10,16 @@ def main():
             if int(end_m) < 0:
                 print("Invalid meter reading")
                 main()
-            else:
-                continue
             if end_m > beg_m:
                 end_m1 = int(end_m)
             else:
                 end_m1 = 1000000000 + int(end_m)
     # calculations
-            if code == "r":
+            if code == "r" or code == "R":
                 gallons = (int(end_m1) - int(beg_m)) / 10
                 bill = 5 + (0.0005 * gallons)
                 bills = f"${round(bill, 2)}"
-            elif code == "c":
+            elif code == "c" or code == " C":
                 gallons = (int(end_m1) - int(beg_m)) / 10
                 gal = gallons - 4000000
                 if gallons <= 4000000:
@@ -29,7 +27,7 @@ def main():
                 elif gallons >= 4000000:
                     bill = 1000 + (0.00025 * gal)
                     bills = f"${format(bill, '.2f')}"
-            elif code == "i":
+            elif code == "i" or code == "I":
                 gallons = (int(end_m1) - int(beg_m)) / 10
                 gal = gallons - 10000000
                 if gallons <= 4000000:
